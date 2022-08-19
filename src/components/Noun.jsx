57828f3176a84b35a2d2bfa7ab351da14ba2ec43
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-19 11:15:22 +0200

    toggleAnswerIsShowing in

--- src/components/Noun.jsx
+++ src/components/Noun.jsx
@@ -1,10 +1,15 @@
 export const Noun = ({ noun }) => {
+
+	const toggleAnswerIsShowing = (noun) => {
+		noun.answerIsShowing = !noun.answerIsShowing;
+	}
+
 	return (
 		<div className="noun">
 			{noun.answerIsShowing && (
 				<div className="article">{noun.article}</div>
 			)}
-			<div className="singular">{noun.singular}</div>
+			<div className="singular" onClick={() => toggleAnswerIsShowing(noun)}>{noun.singular}</div>
 			{noun.answerIsShowing && (
 				<div className="plural">{noun.plural}</div>
 			)}
