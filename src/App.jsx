faa04cd1c103fac5bee91c55eb84c43f0c4a93e5
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-19 10:26:09 +0200

    nouns are showing

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,19 @@ function App() {
 		})();
 	}, []);
 
-
-
 	return (
 		<div className="App">
 			<h2>German Nouns</h2>
 			<p>There are {nouns.length} nouns.</p>
+			{nouns.map((noun, i) => {
+				return (
+					<div className="noun" key={i}>
+						<div className="article">{noun.article}</div>
+						<div className="singular">{noun.singular}</div>
+						<div className="plural">{noun.plural}</div>
+					</div>
+				)
+			})}
 		</div>
 	);
 }
