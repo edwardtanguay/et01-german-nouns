ae0ebc2bc197c1eab5b446bbbcc2f18b1b0748f3
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-19 10:10:49 +0200

    nouns are loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 const baseUrl = 'https://edwardtanguay.netlify.app/share';
@@ -7,12 +7,23 @@ const germanNounsUrl = `${baseUrl}/germanNouns.json`;
 
 function App() {
 	const [nouns, setNouns] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await fetch(germanNounsUrl);
+			const _nouns = await response.json();
+			setNouns(_nouns);
+		})();
+	}, []);
+
+
+
 	return (
 		<div className="App">
 			<h2>German Nouns</h2>
 			<p>There are {nouns.length} nouns.</p>
 		</div>
-	)
+	);
 }
 
-export default App
+export default App;
